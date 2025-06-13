dcf485c66b020343bfcea88e671f095eedca15c0
vipuljainvjj <vipuljain3456@gmail.com>
2025-06-13 22:15:15 +0530

    Update the Initial FIle

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+## print hellow world command
+print("Hello World!")
+
+## if command
+age=32
+print(type(age))
+if (age > 30):
+    ## python do not use {}, it rely on indentation to identify inner code of block
+    print("Age is greater than 30")
+
+name="Vipul Jain"
+print(type(name))
+
